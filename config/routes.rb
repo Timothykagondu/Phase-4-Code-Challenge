c2db206287206af723a5cfb1af4d0d7370232527
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-19 23:21:15 +0300

    started routeing with json

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  
+  resources :restaurants, only: [:index, :show, :destroy]
+  resources :pizzas, only: [:index]
+  resources :restaurant_pizzas, only: [:create]
+  resources :restaurant_pizzas, only: [:create, :index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
