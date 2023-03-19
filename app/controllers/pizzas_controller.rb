1904a5beebebbadae56e3b9de749fd7e3eb1a387
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-19 22:32:41 +0300

    pizza return in json

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -1,6 +1,9 @@
 class PizzasController < ApplicationController
+
     def index
+      
         @pizzas = Pizza.all
         render json: @pizzas.to_json(only: [:id, :name, :ingredients])
       end
+
 end
--- app/controllers/restaurants_controller.rb
+++
@@ -1,2 +0,0 @@
-class RestrauntsController < ApplicationController
-end
