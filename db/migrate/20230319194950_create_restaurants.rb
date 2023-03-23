6af99257848a34831b479dc24b86ff8be24f9d30
TimothyKagondu <timothy.2k21@gmail.com>
2023-03-23 17:10:05 +0300

    fixing errors

--- db/migrate/20230319194950_create_restaurants.rb
+++ db/migrate/20230319194950_create_restaurants.rb
@@ -1,6 +1,6 @@
 class CreateResturants < ActiveRecord::Migration[7.0]
   def change
-    create_table :resturants do |t|
+    create_table :restaurants do |t|
       t.string :name
       t.string :address
 
